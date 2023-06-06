f30d8f79d2c84405f7e70df533808c80debeedd4
GKastet <127600582+GKastet@users.noreply.github.com>
2023-06-06 20:00:22 +0200

    hw-06-10tasks

--- js/task-10.js
+++ js/task-10.js
@@ -4,3 +4,29 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const refs = {
+  divControls: document.querySelector('#controls'),
+  inputNumber: document.querySelector('[type="number"]'),
+  btnCreate: document.querySelector('[data-create]'),
+  btnDestroy: document.querySelector('[data-destroy]'),
+  divBoxes: document.querySelector('#boxes'),
+}
+
+function createBoxes(amount){
+  const divBoxSize = 30;
+  const divArray = [];
+  for(let i = 0; i < amount; i+=1){
+  const divBox = document.createElement('div');
+        divBox.style.width = `${divBoxSize +i*10}px`;
+        divBox.style.height = `${divBoxSize +i*10}px`;
+        divBox.style.backgroundColor = getRandomHexColor();
+        divArray.push(divBox);
+        }
+  refs.divBoxes.append(...divArray);
+}
+
+function destroyBoxes(){
+  refs.divBoxes.innerHTML = '';
+}
+refs.btnDestroy.addEventListener('click', destroyBoxes);
+refs.btnCreate.addEventListener('click', () => createBoxes(refs.inputNumber.value))
